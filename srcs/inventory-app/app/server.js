a9de8d47a35ce65690b50fbff07d2af20a2e5d4b
Zewasik <shumahershik@gmail.com>
2023-12-11 16:33:18 +0200

    add k8s manifest files for inventory app and postgres change api to check connectivity to db at start

--- srcs/inventory-app/app/server.js
+++ srcs/inventory-app/app/server.js
@@ -1,9 +1,42 @@
-const { PORT } = require('./config/config')
+const {
+  DATABASE_USER,
+  DATABASE_PASSWORD,
+  DATABASE_NAME,
+  DATABASE_HOST,
+  DATABASE_PORT,
+} = require("./config/config")
+
+const { PORT } = require("./config/config")
 const express = require("express")
 const cors = require("cors")
 const routes = require("./routes/routes")
 const controllers = require("./controllers/movies")
+const { Client } = require("pg")
+
 const app = express()
+
+const config = {
+  user: DATABASE_USER,
+  host: DATABASE_HOST,
+  database: DATABASE_NAME,
+  password: DATABASE_PASSWORD,
+  port: DATABASE_PORT,
+}
+
+const client = new Client(config)
+
+client
+  .connect()
+  .then(() => {
+    console.log("Connected to PostgreSQL database")
+  })
+  .catch((err) => {
+    console.error("Error connecting to PostgreSQL database:", err.message)
+    process.exit(1)
+  })
+  .finally(() => {
+    client.end()
+  })
 
 app.use(cors(), express.json())
 app.use("/api", routes(controllers))
