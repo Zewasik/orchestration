57a6211eaa12a6b00e05300547b2d0db5947297f
Zewasik <shumahershik@gmail.com>
2023-12-12 02:35:13 +0200

    add manifest files for billing app, postgres and rabbitmq change billing server to check conection to pg on start

--- srcs/billing-app/app/server.js
+++ srcs/billing-app/app/server.js
@@ -1,6 +1,39 @@
-const { RABBITMQ_QUEUE_NAME, RABBITMQ_HOST_ADDRESS } = require('./config/config')
-const amqplib = require('amqplib/callback_api')
+const {
+  DATABASE_USER,
+  DATABASE_PASSWORD,
+  DATABASE_NAME,
+  DATABASE_HOST,
+  DATABASE_PORT,
+  RABBITMQ_QUEUE_NAME,
+  RABBITMQ_HOST_ADDRESS,
+} = require("./config/config")
+const amqplib = require("amqplib/callback_api")
 const controllers = require("./controllers/orders")
+
+const { Client } = require("pg")
+
+const config = {
+  user: DATABASE_USER,
+  host: DATABASE_HOST,
+  database: DATABASE_NAME,
+  password: DATABASE_PASSWORD,
+  port: DATABASE_PORT,
+}
+
+const client = new Client(config)
+
+client
+  .connect()
+  .then(() => {
+    console.log("Connected to PostgreSQL database")
+  })
+  .catch((err) => {
+    console.error("Error connecting to PostgreSQL database:", err.message)
+    process.exit(1)
+  })
+  .finally(() => {
+    client.end()
+  })
 
 amqplib.connect(`amqp://${RABBITMQ_HOST_ADDRESS}`, (err, conn) => {
   if (err) throw err
@@ -9,27 +42,29 @@ amqplib.connect(`amqp://${RABBITMQ_HOST_ADDRESS}`, (err, conn) => {
     if (err) throw err
 
     channel.assertQueue(RABBITMQ_QUEUE_NAME)
-    channel.consume(RABBITMQ_QUEUE_NAME, (msg) => {
-      const acknowledge = () => channel.ack(msg)
-      const notAcknowledge = () => channel.nack(msg, false, false)
+    channel.consume(
+      RABBITMQ_QUEUE_NAME,
+      (msg) => {
+        const acknowledge = () => channel.ack(msg)
+        const notAcknowledge = () => channel.nack(msg, false, false)
 
-      if (msg !== null) {
-        try {
-          const jsonData = JSON.parse(msg.content.toString())
+        if (msg !== null) {
+          try {
+            const jsonData = JSON.parse(msg.content.toString())
 
-          if (typeof jsonData === "object") {
-            controllers.createOrder(jsonData, acknowledge, notAcknowledge)
-          } else {
-            throw new Error("Received message is not a JSON object.")
+            if (typeof jsonData === "object") {
+              controllers.createOrder(jsonData, acknowledge, notAcknowledge)
+            } else {
+              throw new Error("Received message is not a JSON object.")
+            }
+          } catch (error) {
+            console.error("Error:", error)
+            notAcknowledge()
           }
-        } catch (error) {
-          console.error("Error:", error)
-          notAcknowledge()
+        } else {
+          console.log("Consumer cancelled by server")
         }
-      } else {
-        console.log("Consumer cancelled by server")
-      }
-    },
+      },
       { noAck: false }
     )
   })
